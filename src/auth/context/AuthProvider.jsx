a57e6051c2f7621457ab5b9ffb424e111bd2f355
davidperezboza <david.perez@babelgroup.com>
2025-01-23 22:04:21 +0100

    Video 213

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,5 +1,6 @@
-import { useReducer } from "react";
-import { AuthContext, AuthReducer } from "./";
+import { useReducer } from 'react';
+import { AuthContext, AuthReducer } from './';
+import { types } from '../types';
 
 const initialState = {
     logged: false,
@@ -7,8 +8,20 @@ const initialState = {
 
 export const AuthProvider = ({children}) => {
     const [authState, dispatch] = useReducer(AuthReducer, initialState);
+
+    const login = (name = '') => {
+        const action = {
+            type: types.login,
+            payload: {
+                id: 'ABC',
+                name: name,
+            },
+        }
+        console.log(action.payload);
+        dispatch(action);
+    }
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{...authState, login}}>
             {children}
         </AuthContext.Provider>
     );
