1b28f5bb43d8fdce7e771f51bacdae89e828bdd9
davidperezboza <david.perez@babelgroup.com>
2025-01-24 11:37:02 +0100

    Video 217

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,23 @@
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth/';
 import { HeroesRoutes } from '../heroes/';
-import { PrivateRoute } from './';
+import { PrivateRoute, PublicRoute } from './';
 
 export const AppRouter = () => {
   return (
     <>
         <Routes>
-            <Route path="login" element={<LoginPage />} />
-            <Route path='/*' element={
-              <PrivateRoute >
-                  <HeroesRoutes />
-              </PrivateRoute>} 
-            />
+          <Route path="login" element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute> 
+          }
+          />
+          <Route path='/*' element={
+            <PrivateRoute >
+              <HeroesRoutes />
+            </PrivateRoute>} 
+          />
         </Routes>
     </>
   );
